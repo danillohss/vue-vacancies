6fbbdd8754d24dc669fe4dff0bcbe30c6037220b
Danillo Soares <Danillohss@gmail.com>
2023-04-10 12:18:32 -0300

    correções

--- src/components/comuns/ListaVagas.vue
+++ src/components/comuns/ListaVagas.vue
@@ -1,16 +1,10 @@
 <template>
-    <slot name="titulo" :dadosTitulo="{ titulo: 'Titulo', nroVagas: 15 }">
-        <p>Título da lista de vagas </p>
-    </slot>
-    <slot atributo1=" att do slot filho para o pai" :atributo2='[1, 2, 3, 4, 5]'>
+    <slot>
         <div v-for="(vaga, index) in vagas" :key="index" class="row mt-5">
             <div class="col">
                 <Vaga v-bind="vaga" />
             </div>
         </div>
-    </slot>
-    <slot :dadosRodape="{ titulo: 'RodapeLista', paginacao: { nPaginas: 3, paginaAtual: 2 } }">
-        <p>Rodapé da lista</p>
     </slot>
 </template>
 
