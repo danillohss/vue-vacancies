148d55fd536abba0da05658505031ab13518c9aa
Danillo Soares <danillohss@gmail.com>
2023-04-27 18:45:54 -0300

    fix no modal de exclusao e vagas disponiveis

--- src/components/comuns/ListaVagas.vue
+++ src/components/comuns/ListaVagas.vue
@@ -2,58 +2,63 @@
   <div v-for="vaga in vagas" :key="vaga.id" class="row mt-5">
     <div class="col">
       <Vaga v-bind="vaga" />
-      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <div class="d-grid gap-2 d-md-flex justify-content-md-top">
         <button
           class="btn btn-danger btn-sm"
-          @click="(this.confirmarExclusao = true), deleteVaga(vaga.id)"
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          @click="deleteVaga(vaga.id, vaga.titulo)"
         >
           Deletar vaga
         </button>
-      </div>
-      <!-- <div
-      type="button"
-      class="btn btn-danger btn-sm"
-      data-bs-toggle="modal"
-      data-bs-target="#exampleModal"
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Deseja realmente excluir a vaga selecionada ?
-              </h1>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Cancelar
-              </button>
-              <button
-                type="button"
-                class="btn btn-danger"
-                data-bs-dismiss="modal"
-                @click="(this.confirmarExclusao = true), deleteVaga(vaga.id)"
-              >
-                Excluir id: {{ vaga.id }}
-              </button>
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                  Deseja excluir?
+                </h1>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                Confirme a exclusão da vaga
+                <strong>
+                  {{ tituloVaga }}
+                </strong>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Cancelar
+                </button>
+                <button
+                  data-bs-dismiss="modal"
+                  type="button"
+                  class="btn btn-primary"
+                  @click="modalButton()"
+                >
+                  Excluir
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -69,19 +74,24 @@ export default {
       vagas: [],
       confirmarExclusao: false,
       idVaga: null,
+      tituloVaga: "",
     };
   },
   methods: {
-    deleteVaga(id) {
+    deleteVaga(id, titulo) {
+      this.idVaga = id;
+      this.tituloVaga = titulo;
       if (this.confirmarExclusao === true) {
         let filtro = this.vagas.filter((item) => {
           return item.id !== id;
         });
         this.confirmarExclusao = false;
-        console.log("IF");
         return (this.vagas = filtro);
       }
-      console.log("else");
+    },
+    modalButton() {
+      this.confirmarExclusao = true;
+      this.deleteVaga(this.idVaga, this.tituloVaga);
     },
   },
   activated() {
@@ -105,3 +115,5 @@ export default {
   },
 };
 </script>
+
+
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,51 +1,69 @@
 <template>
-    <div class="container py-4">
-        <div class="row">
-            <div class="col">
-                <PesquisarVaga />
-            </div>
-        </div>
-        <ListaVagas />
-        <div class="row mt-5">
-            <div class="col-4">
-                <Indicador titulo="Vagas disponíveis" :indicador="indicador" bg="bg-dark" color="text-white" />
-            </div>
-            <div class="col-4">
-                <Indicador titulo="Profissionais cadastrados" indicador="225" bg="bg-dark" color="text-white" />
-            </div>
-            <div class="col-4">
-                <Indicador titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-dark" color="text-white" />
-            </div>
-        </div>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <PesquisarVaga />
+      </div>
     </div>
+    <ListaVagas />
+    <div class="row mt-5">
+      <div class="col-4">
+        <Indicador
+          titulo="Vagas disponíveis"
+          :indicador="indicador"
+          bg="bg-dark"
+          color="text-white"
+        />
+      </div>
+      <div class="col-4">
+        <Indicador
+          titulo="Profissionais cadastrados"
+          indicador="225"
+          bg="bg-dark"
+          color="text-white"
+        />
+      </div>
+      <div class="col-4">
+        <Indicador
+          titulo="Visitantes online"
+          :indicador="usuariosOnline"
+          bg="bg-dark"
+          color="text-white"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue'
-import ListaVagas from '@/components/comuns/ListaVagas.vue'
-import Indicador from '@/components/comuns/Indicador.vue'
+import PesquisarVaga from "@/components/comuns/PesquisarVaga.vue";
+import ListaVagas from "@/components/comuns/ListaVagas.vue";
+import Indicador from "@/components/comuns/Indicador.vue";
 export default {
-    // eslint-disable-next-line
-    name: 'Home',
-    components: { PesquisarVaga, Indicador, ListaVagas },
-    created() {
-        setInterval(this.getUsuariosOnline, 10000);
+  // eslint-disable-next-line
+  name: "Home",
+  components: { PesquisarVaga, Indicador, ListaVagas },
+  created() {
+    setInterval(this.getUsuariosOnline, 10000);
+    setInterval(this.getIndicador, 1);
+  },
+  data() {
+    return {
+      indicador: 0,
+      usuariosOnline: 26,
+    };
+  },
+  methods: {
+    getUsuariosOnline() {
+      this.usuariosOnline = Math.floor(Math.random() * 101);
     },
-    activated() {
-            this.indicador = JSON.parse(localStorage.getItem('vagas')).length
+    getIndicador() {
+      JSON.parse(localStorage.getItem("vagas")) === null
+        ? (this.indicador = 0)
+        : (this.indicador = JSON.parse(localStorage.getItem("vagas")).length);
     },
-    data() {
-        return {
-            indicador: 0,
-            usuariosOnline: 22,
-        }
-    },
-    methods: {
-        getUsuariosOnline() {
-            this.usuariosOnline = Math.floor(Math.random() * 101)
-        },
-    },
-}
+  },
+};
 </script>
 
 <style scoped></style>
